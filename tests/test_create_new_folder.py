90915af58f5c5b8827624ed5391b87926ee24a6c
spacebroth <85671590+spacebroth@users.noreply.github.com>
2022-11-07 06:51:19 +0300

    Update test_create_new_folder.py

--- tests/test_create_new_folder.py
+++ tests/test_create_new_folder.py
@@ -1,22 +1,14 @@
 import pytest
-import requests
-
 from pages.api import DiskApi
 
 
 @pytest.mark.parametrize('path', ['?path='])
 class TestApiDisk(DiskApi):
-
-    # def teardown(self):
-    #     self.delete_file('?path=')
-
     def test_create_named_folder(self, path):
         try:
-            status_code = self.create_named_folder(path).status_code
-            self.delete_file(path)
-            body = self.create_named_folder(path).json()
+            res = self.create_named_folder(path)
             assert ('name', 'API_Folder') in self.get_file_name(path), 'Папка не была создана'
-            assert status_code == 201, 'Код ответа не соответствует ожидаемому'
-            assert body == self.body, 'Тело ответа не соответствует требованиям'
+            assert res.status_code == 201, 'Код ответа не соответствует ожидаемому'
+            assert res.json() == self.body, 'Тело ответа не соответствует требованиям'
         finally:
             self.delete_file('?path=')
